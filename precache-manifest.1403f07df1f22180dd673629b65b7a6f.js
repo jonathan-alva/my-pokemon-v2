fe35cb95c78f5074af7887c95a9a3cd247aa55ca
Jonathan Alva <exzone97@yahoo.co.id>
2020-08-10 09:32:28 +0700

    Updates

---
+++ precache-manifest.1403f07df1f22180dd673629b65b7a6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bb0a122b459448dab5d3511f3f4b3e",
+    "url": "/my-pokemon-v2/index.html"
+  },
+  {
+    "revision": "128ab0d6826b25c483f7",
+    "url": "/my-pokemon-v2/static/css/main.3635f793.chunk.css"
+  },
+  {
+    "revision": "a7e27c51049576ca5ebe",
+    "url": "/my-pokemon-v2/static/js/2.ee061c5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-pokemon-v2/static/js/2.ee061c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128ab0d6826b25c483f7",
+    "url": "/my-pokemon-v2/static/js/main.cd0a43f1.chunk.js"
+  },
+  {
+    "revision": "522bdea222038528df93",
+    "url": "/my-pokemon-v2/static/js/runtime-main.959895fa.js"
+  },
+  {
+    "revision": "e56aac5d0b078942203a9efe5655aae1",
+    "url": "/my-pokemon-v2/static/media/height.e56aac5d.svg"
+  },
+  {
+    "revision": "fc273ffe3e1d90445fd1e8a11b8c4727",
+    "url": "/my-pokemon-v2/static/media/pokeball.fc273ffe.png"
+  },
+  {
+    "revision": "20a06cc019642cd1fa8c6dc99a962bc4",
+    "url": "/my-pokemon-v2/static/media/weight.20a06cc0.svg"
+  }
+]);
